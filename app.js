9095fa86382585e8b9e7149711ca1171160d0f74
jang_jiwon <kooriangman@gmail.com>
2023-03-27 10:42:34 +0900

    [MODIFY] app파일, server파일 분리

--- app.js
+++ app.js
@@ -1,35 +1,19 @@
-require("dotenv").config();
-
 const express = require("express");
 const cors = require("cors");
 const morgan = require("morgan");
 
-const { appDataSource } = require("./models/data-source");
 const { globalErrorHandler } = require("./utils/error");
 
-const app = express();
+const createApp = () => {
+  const app = express();
 
-app.use(express.json());
-app.use(cors());
-app.use(morgan("combined"));
-app.use(globalErrorHandler);
+  app.use(express.json());
+  app.use(cors());
+  app.use(morgan("combined"));
 
-app.get("/ping", (req, res) => {
-  res.status(200).json({ message: "pong" });
-});
+  app.use(globalErrorHandler);
 
-const PORT = process.env.PORT;
-
-const start = async () => {
-  try {
-    await appDataSource.initialize().then(() => console.log("DB Connection"));
-
-    app.listen(PORT, () =>
-      console.log(`🥥🥥Server is listening on ${PORT}🥥🥥`)
-    );
-  } catch (err) {
-    console.error(err);
-  }
+  return app;
 };
 
-start();
+module.exports = { createApp };
---
+++ server.js
@@ -0,0 +1,23 @@
+require("dotenv").config();
+
+const { createApp } = require("./app");
+const { appDataSource } = require("./models/data-source");
+
+const start = async () => {
+  const app = createApp();
+  const PORT = process.env.PORT;
+  try {
+    await appDataSource.initialize();
+    console.log("DB Connection");
+    app.get("/ping", (req, res) => {
+      res.status(200).json({ message: "pong" });
+    });
+    app.listen(PORT, () =>
+      console.log(`🥥🥥Server is listening on ${PORT}🥥🥥`)
+    );
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+start();
