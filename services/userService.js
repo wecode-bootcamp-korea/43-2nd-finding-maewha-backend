55b2230f3fc24f93b3b20ad2249e5e9fe90596df
jang_jiwon <kooriangman@gmail.com>
2023-03-29 15:25:17 +0900

    [MODIFY] 에러 핸들링 수정

--- services/userService.js
+++ services/userService.js
@@ -4,35 +4,32 @@ const axios = require("axios");
 const userDao = require("../models/userDao");
 
 const kakaoSignIn = async (kakaoToken) => {
-  const getKakaoToken = await axios.get("https://kapi.kakao.com/v2/user/me", {
-    headers: {
-      authorization: `Bearer ${kakaoToken}`,
-      "Content-type": "application/x-www-form-urlencoded;charset=utf-8",
-    },
-  });
+  const getKakaoUser = await axios
+    .get("https://kapi.kakao.com/v2/user/me", {
+      headers: {
+        authorization: `Bearer ${kakaoToken}`,
+        "Content-type": "application/x-www-form-urlencoded;charset=utf-8",
+      },
+    })
+    .catch((err) => {
+      const error = new Error("KAKAO_TOKEN_ERROR");
+      error.statusCode = 400;
+      throw error;
+    });
 
-  if (!getKakaoToken) {
-    const error = new Error("KAKAO_TOKEN_ERROR");
-    error.statusCode = 400;
-
-    throw error;
-  }
-
-  const { data } = getKakaoToken;
+  const { data } = getKakaoUser;
   console.log(data);
   const kakaoId = data.id;
   const name = data.properties.nickname;
   const email = data.kakao_account.email;
   const gender = data.kakao_account.gender;
-  const userId = await userDao.checkUserByKakaoId(kakaoId);
+  let user = await userDao.checkUserByKakaoId(kakaoId);
 
-  if (!userId) {
-    const newUser = await userDao.createUser(email, name, kakaoId, gender);
-
-    return jwt.sign({ userId: newUser.insertId }, process.env.JWT_SECRET);
+  if (!user) {
+    user = await userDao.createUser(email, name, kakaoId, gender);
   }
 
-  return jwt.sign({ userId: userId }, process.env.JWT_SECRET);
+  return jwt.sign({ userId: user.id }, process.env.JWT_SECRET);
 };
 
 module.exports = {
